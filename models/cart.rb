cf877182c1805fd647c3770fc21cdf72ae35bd32
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-10 22:13:01 +0000

    Automatically backed up by Learn

---
+++ models/cart.rb
@@ -0,0 +1,16 @@
+class Cart
+  attr_reader :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+end
